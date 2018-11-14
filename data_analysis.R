09561351dadb9b2833ebadb8a5b1408edaa40ea5
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2018-11-14 16:32:52 -0500

    Removed many comments in script, added $$ around inline \Sexpr's, removed all unused bib sources

--- data_analysis.R
+++ data_analysis.R
@@ -9,14 +9,12 @@ require("knitr")
 require("plyr")
 
 # Test of independance between nIche and niCHE
-# Summary of counts in a table and in the margins
 ## ---- niche_niche_table ----
 nIche_by_niCHE <- table(data_cleaned$nIche,
                         data_cleaned$niCHE)
 kable(addmargins(nIche_by_niCHE),
       format = "latex")
 
-# Chi-square test for independance for nIche by niCHE, expected counts and results
 ## ---- niche_niche_chisq_perform ----
 nIche_by_niCHE_chisq_result <- chisq.test(nIche_by_niCHE)
 ## ---- niche_niche_chisq_expected ----
@@ -28,11 +26,8 @@ nIche_by_niCHE_chisq_result
 ## ---- niche_niche_chisq_residuals ----
 kable(nIche_by_niCHE_chisq_result$residuals,
       format = "latex",
-      digits = 2) # Actually sqrt of the residuals
-# Ultimately shows that these variables are highly dependant, and so we can look at
-# either one with the other factors and expect the same results. We'll use niCHE
+      digits = 2)
 
-# Overall distribution of niCHE
 ## ---- niche_overall_barplot_make ----
 niCHE_overall <- table(data_cleaned$niCHE)
 ## ---- niche_overall_barplot_view ----
@@ -46,12 +41,10 @@ text(niCHE_overall_barplot,
      label = niCHE_overall,
      pos = 3)
         
-# Test of independance between Age and niCHE
-# Exclude those with unknown ages
 ## ---- age_niche_table ----
+# Exclude those with unknown ages
 data_cleaned_sans_unknown_ages <- droplevels(data_cleaned[data_cleaned$Age != "Unknown",])
 
-# Summary of counts in a table and in the margins, excluding unknown ages
 niCHE_by_Age <- table(data_cleaned_sans_unknown_ages$niCHE,
                             data_cleaned_sans_unknown_ages$Age)
 colnames(niCHE_by_Age) <- c("20 or Younger",
@@ -60,29 +53,24 @@ colnames(niCHE_by_Age) <- c("20 or Younger",
 kable(addmargins(niCHE_by_Age),
       format = "latex")
 
-# Chi-square test for independance for Age by niCHE, expected counts and results
 ## --- age_niche_chisq_perform ----
 niCHE_by_Age_chisq_result <- chisq.test(niCHE_by_Age)
 ## --- age_niche_chisq_expected ----
 kable(niCHE_by_Age_chisq_result$expected,
       format = "latex",
-      digits = 2) # Consevatively, chi-square shouldn't be used
-# Perform Fisher's exact test instead
+      digits = 2)
+
 ## ---- age_niche_fish_perform ----
 niCHE_by_Age_Fish_result <- fisher.test(niCHE_by_Age)
 ## ---- age_niche_fish_result ----
 niCHE_by_Age_Fish_result
-# The result is not significant by any likely alpha
         
-# Test of independance between Gender and niCHE
-# Summary of counts in a table and in the margins
 ## ---- gender_niche_table ----
 niCHE_by_Gender <- table(data_cleaned$niCHE,
                          data_cleaned$Gender)
 kable(addmargins(niCHE_by_Gender),
       format = "latex")
 
-# Chi-square test for independance for Gender by niCHE, expected counts and results
 ## ---- gender_niche_chisq_perform ----
 niCHE_by_Gender_chisq_result <- chisq.test(niCHE_by_Gender)
 ## ---- gender_niche_chisq_expected ----
@@ -91,32 +79,25 @@ kable(niCHE_by_Gender_chisq_result$expected,
       digits = 2)
 ## ---- gender_niche_chisq_result ----
 niCHE_by_Gender_chisq_result
-# The result is not significant by any likely alpha
     
-# Test of independance between Ethnicity and niCHE
-# Summary of counts in a table and in the margins
 ## ---- ethnicity_niche_table ----
 niCHE_by_Ethnicity <- table(data_cleaned$niCHE,
                          data_cleaned$Ethnicity)
 kable(addmargins(niCHE_by_Ethnicity),
       format = "latex")
 
-# Chi-square test for independance for Ethnicity by niCHE, expected counts and results
 ## ---- ethnicity_niche_chisq_perform ----
 niCHE_by_Ethnicity_chisq_result <- chisq.test(niCHE_by_Ethnicity)
 ## ---- ethnicity_niche_chisq_expected ----
 kable(niCHE_by_Ethnicity_chisq_result$expected,
       format = "latex",
-      digits = 2) # Definitely not appropriate to use chi-square
-# Perform Fisher's exact test instead
+      digits = 2)
+
 ## ---- ethnicity_niche_fish_perform ----
 niCHE_by_Ethnicity_Fish_result <- fisher.test(niCHE_by_Ethnicity)
 ## ---- ethnicity_niche_fish_result ----
 niCHE_by_Ethnicity_Fish_result
-# The result is not significant by any likely alpha
 
-# Test of independance between Location and niCHE
-# Summary of counts in a table and in the margins
 ## ---- location_niche_table ----
 niCHE_by_Location <- table(data_cleaned$niCHE,
                          data_cleaned$Location)
@@ -126,7 +107,6 @@ colnames(niCHE_by_Location) <- c("Downtown",
 kable(addmargins(niCHE_by_Location),
       format = "latex")
 
-# Chi-square test for independance for Location by niCHE, expected counts and results
 ## ---- location_niche_chisq_perform ----
 niCHE_by_Location_chisq_result <- chisq.test(niCHE_by_Location)
 ## ---- location_niche_chisq_expected ----
@@ -135,36 +115,29 @@ kable(niCHE_by_Location_chisq_result$expected,
       digits = 2)
 ## ---- location_niche_chisq_result ----
 niCHE_by_Location_chisq_result
-# The result is not significant by any likely alpha
 
-# Test of independance between Year and niCHE
-# Exclude those with unknown ages or who were non-students, the non-students only numbering 2
 ## ---- year_niche_table ----
+# Exclude those with unknown ages or who were non-students, the non-students only numbering 2
 data_cleaned_sans_unknown_and_nonstudent_years <- droplevels(data_cleaned[data_cleaned$Year != "Unknown" & data_cleaned$Year != "Not_student",])
 
-# Summary of counts in a table and in the margins
 niCHE_by_Year <- table(data_cleaned_sans_unknown_and_nonstudent_years$niCHE,
                        data_cleaned_sans_unknown_and_nonstudent_years$Year)
 addmargins(niCHE_by_Year)
 kable(addmargins(niCHE_by_Year),
       format = "latex")
 
-# Chi-square test for independance for Year by niCHE, expected counts and results
 ## ---- year_niche_chisq_perform ----
 niCHE_by_Year_chisq_result <- chisq.test(niCHE_by_Year)
 ## ---- year_niche_chisq_expected ----
 kable(niCHE_by_Year_chisq_result$expected,
       format = "latex",
-      digits = 2) # Pretty inappropriate to use chi-square
-# Perform Fisher's exact test instead
+      digits = 2)
+
 ## ---- year_niche_fish_perform ----
 niCHE_by_Year_Fish_result <- fisher.test(niCHE_by_Year)
 ## ---- year_niche_fish_result ----
 niCHE_by_Year_Fish_result
-# The result is not significant by any likely alpha
-    
-# There seems to be a pattern, though.
-# Create a side by side bar graph for Year by niCHE to show it
+
 ## ---- year_niche_barplot_make ----
 niCHE_by_Year_props <- prop.table(niCHE_by_Year,
                                   2)
