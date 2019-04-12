979a60203670724d8ae8afd8e7b1bbfac415fa68
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-04-12 11:38:33 -0400

    Initial outline

---
+++ quant_meths_homework6/homework6.R
@@ -0,0 +1,30 @@
+############
+# Preamble #
+############
+
+## ---- load_libraries_data ----
+# Load packages
+
+# Load the dataset
+ff <- read.csv("fourthFloor.csv",
+               header = TRUE)
+
+# Convert speaker to a factor instead of numeric
+
+##########
+# Models #
+##########
+
+
+##########
+# Graphs #
+##########
+
+## ---- some_plot ----
+
+
+##########
+# Tables #
+##########
+
+## ---- some_table ----
