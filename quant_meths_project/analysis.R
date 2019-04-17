20a5465507c09116eeae2bf4a0e14b53f217ee78
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-04-17 00:46:14 -0400

    First and maybe final draft

--- quant_meths_project/analysis.R
+++ quant_meths_project/analysis.R
@@ -10,16 +10,56 @@
 # Read in data
 tv <- read.csv("nccfr_tv_cleaned.csv")
 
+# Load packages
+library(rms)
+library(plyr)
+
 ################
 #   Analyses   #
 ################
 
-# Binomial logistic regression
+# Create an initial model with all the factors included
+tv.lrm <- lrm(TV ~
+                GENDER +
+                REGION +
+                AGE,
+              data = tv)
 
-################
-#    Tables    #
-################
+# Use the anova function to extract coefficient P-values
+tv.lrm.anova <- anova(tv.lrm)
+
+# Create a glm version so as to use stepwise factor selection
+tv.glm <- glm(TV ~
+                GENDER +
+                REGION +
+                AGE,
+              data = tv,
+              family = binomial)
+
+# Use step-down selection
+tv.glm.bw <- step(tv.glm,
+                  direction = "backward",
+                  trace = FALSE)
+
+# Add x and y to the lrm model to test for overfitting
+tv.lrm.plus <- lrm(TV ~
+                     GENDER,
+                   data = tv,
+                   x = TRUE,
+                   y = TRUE)
+
+tv.lrm.plus.val <- validate(tv.lrm.plus,
+                            B = 200)
+
+tv.lrm.final <- lrm(TV ~
+                      GENDER,
+                    data = tv)
 
 ################
 #    Graphs    #
 ################
+
+## ---- final_model ----
+ggplot(tv,
+       aes(x = GENDER)) +
+  geom_bar()
