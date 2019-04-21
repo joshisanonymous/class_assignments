e0c54d30e41dde801369c0f8fe5ee15a9a43e81f
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-04-20 23:12:24 -0400

    Completed data cleaning

--- socio_seminar_project/cleaning.R
+++ socio_seminar_project/cleaning.R
@@ -9,18 +9,42 @@
 ## ---- clean_data ----
 # Read in the data
 tp <- read.csv("third_person_raw.csv",
-               header = FALSE)
+               header = TRUE,
+               encoding = "UTF-8")
 
-# Add names to the columns
-colnames(tp) <- c()
+# Collapse c', ç', and ça to just ça
+tp$PRONOUN <- as.factor(gsub("c'",
+                             "ça",
+                             tp$PRONOUN))
+tp$PRONOUN <- as.factor(gsub("ç'",
+                             "ça",
+                             tp$PRONOUN))
 
-# Collapse ...
-tp$... <- as.factor(gsub("...",
-                        "...",
-                        tp$...))
-
-# Coerce ... to a factor
-tp$... <- as.factor(tp$...)
+# Substitute positional tags to readable values
+tp$SPECIFICITY <- as.factor(gsub("RGE",
+                                 "generic",
+                                 tp$SPECIFICITY))
+tp$SPECIFICITY <- as.factor(gsub("RSP",
+                                 "specific",
+                                 tp$SPECIFICITY))
+tp$ANIMACY <- as.factor(gsub("RAN",
+                             "animate",
+                             tp$ANIMACY))
+tp$ANIMACY <- as.factor(gsub("RIN",
+                             "inanimate",
+                             tp$ANIMACY))
+tp$REF.DISTANCE <- as.factor(gsub("ITU",
+                                  "in.t.unit",
+                                  tp$REF.DISTANCE))
+tp$REF.DISTANCE <- as.factor(gsub("OTU",
+                                  "out.t.unit",
+                                  tp$REF.DISTANCE))
+tp$VERB.TYPE <- as.factor(gsub("VEA",
+                               "etre.avoir",
+                               tp$VERB.TYPE))
+tp$VERB.TYPE <- as.factor(gsub("VRB",
+                               "regular",
+                               tp$VERB.TYPE))
 
 # Save completed data frame as a new csv
 write.csv(tp,
