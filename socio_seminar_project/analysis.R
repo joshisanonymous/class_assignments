15a73a17280b0b65be67cd501853148ac3145f5f
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-04-23 15:26:49 -0400

    Almost final draft of presentation

--- socio_seminar_project/analysis.R
+++ socio_seminar_project/analysis.R
@@ -14,12 +14,68 @@ tp <- read.csv("third_person_cleaned.csv")
 
 # Load packages
 library(plyr)
+library(ggplot2)
+library(rms)
+library(lme4)
 
 ################
 #   Analyses   #
 ################
 
+# Initial model with all factors included
+tp.glmer <- glmer(PRONOUN ~
+                    (1|SPEAKER) +
+                    VERB.TYPE +
+                    ANIMACY +
+                    SPECIFICITY +
+                    REF.DISTANCE,
+                  data = tp,
+                  family = binomial)
+
+# Create object just for model coefs to make extracting
+# values easier
+tp.glmer.coefs <- summary(tp.glmer)$coef
+
+# Removed non-significant factors (i.e., specificity)
+tp.glmer.final <- glmer(PRONOUN ~
+                          (1|SPEAKER) +
+                          VERB.TYPE +
+                          REF.DISTANCE,
+                        data = tp,
+                        family = binomial)
+
+# Create object just for the final model coefs to make
+# extracting values easier
+tp.glmer.final.coefs <- summary(tp.glmer.final)$coef
 
 ################
 #    Graphs    #
 ################
+
+## ---- dist_overall ----
+ggplot(tp,
+       aes(x = PRONOUN)) +
+  theme_bw(8) +
+  labs(x = "3rd Person Plural Subject Pronoun",
+       y = "Frequency") +
+  geom_bar()
+
+## ---- dist_by_speaker ----
+ggplot(tp,
+       aes(x = PRONOUN)) +
+  facet_wrap(. ~ SPEAKER,
+             nrow = 2,
+             ncol = 2) +
+  theme_bw(8) +
+  labs(x = "3rd Person Plural Subject Pronoun",
+       y = "Frequency") +
+  geom_bar()
+
+## ---- dist_by_verb_and_dist ----
+ggplot(tp,
+       aes(x = PRONOUN)) +
+  facet_grid(REF.DISTANCE ~ VERB.TYPE) +
+  theme_bw(8) +
+  labs(x = "3rd Person Plural Subject Pronoun",
+       y = "Frequency") +
+  geom_bar()
--- socio_seminar_project/cleaning.R
+++ socio_seminar_project/cleaning.R
@@ -34,10 +34,10 @@ tp$ANIMACY <- as.factor(gsub("RIN",
                              "Inanimate Referent",
                              tp$ANIMACY))
 tp$REF.DISTANCE <- as.factor(gsub("ITU",
-                                  "Referent in the T-unit",
+                                  "In the T-unit",
                                   tp$REF.DISTANCE))
 tp$REF.DISTANCE <- as.factor(gsub("OTU",
-                                  "Referent not in the T-unit",
+                                  "Not in the T-unit",
                                   tp$REF.DISTANCE))
 tp$VERB.TYPE <- as.factor(gsub("VEA",
                                "Être or Avoir Verb",
@@ -60,7 +60,32 @@ tp$SPEAKER <- as.factor(gsub("ward",
                              "Ward",
                              tp$SPEAKER))
 
+# Insert social variables for each speaker
+# Gender
+tp$GENDER <- "Male"
+# Age
+tp$AGE[tp$SPEAKER == "Coulson"] <- ""
+tp$AGE[tp$SPEAKER == "Talbot"] <- ""
+tp$AGE[tp$SPEAKER == "Fritz"] <- ""
+tp$AGE[tp$SPEAKER == "Ward"] <- ""
+# Current residence
+tp$LOCATION[tp$SPEAKER == "Coulson"] <- "Baton Rouge"
+tp$LOCATION[tp$SPEAKER == "Talbot"] <- "Youngsville"
+tp$LOCATION[tp$SPEAKER == "Fritz"] <- "Baton Rouge"
+tp$LOCATION[tp$SPEAKER == "Ward"] <- "New Orleans"
+# First language
+tp$LANGUAGE[tp$SPEAKER == "Coulson" | tp$SPEAKER == "Talbot"] <- "French"
+tp$LANGUAGE[tp$SPEAKER == "Fitz" | tp$SPEAKER == "Ward"] <- "English"
+  
+# Remove eux and yé from the data as they each occur only once
+tp <- tp[tp$PRONOUN != "eux" & tp$PRONOUN != "yé",]
+
+# Remove all levels with zero counts
+tp <- droplevels(tp)
+
 # Save completed data frame as a new csv
 write.csv(tp,
           file = "third_person_cleaned.csv",
           row.names = FALSE)
+
+rm(tp)
