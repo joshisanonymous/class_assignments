f54feeb0aa665f21a7bec0aa749819bcc61a081c
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-08-26 22:39:15 -0400

    Finished first NLP homework

--- nlp_homework01/nlp_homework01.py
+++
@@ -1,9 +0,0 @@
-from nltk.book import *
-
-# Q1
-# text4.concordance("fathers")
-# text4.concordance("father")
-# text4.count("monstrous")
-text2.concordance("monstrous")
-text1.concordance("monstrous")
-# text4.concordance("lived")
