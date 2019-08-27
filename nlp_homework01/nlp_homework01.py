e8b0433ff062db6453d90a0b44310a145d3cde39
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-08-26 21:04:30 -0400

    Initialization of first NLP homework

--- nlp_homework01/nlp_homework01.py
+++ nlp_homework01/nlp_homework01.py
@@ -1,9 +1,9 @@
 from nltk.book import *
 
 # Q1
-# suggested search
-text4.concordance("fathers")
-text4.concordance("father")
-
-# my searches
-text4.concordance("")
+# text4.concordance("fathers")
+# text4.concordance("father")
+# text4.count("monstrous")
+text2.concordance("monstrous")
+text1.concordance("monstrous")
+# text4.concordance("lived")
