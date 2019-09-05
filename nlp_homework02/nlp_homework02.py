b18258fd6440ecc2454fa2441177815b841786c0
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-09-04 22:01:19 -0400

    Finished nlp_homework02

--- nlp_homework02/nlp_homework02.py
+++ nlp_homework02/nlp_homework02.py
@@ -18,3 +18,23 @@ negPattern = re.compile(r"\W(in|im|ir|il|non|un)\w{2,}")
 negMatches = [match.group() for match in negPattern.finditer(review) if match]
 pairPattern = re.compile(r"\w{2,}ly\s+\w{1,8}\W")
 pairMatches = [match.group() for match in pairPattern.finditer(review) if match]
+
+# Q3
+from nltk.corpus import gutenberg
+alice = gutenberg.raw("carroll-alice.txt")
+pastprogPattern = re.compile(r"(was|were)\s+\w+ing")
+pastprogMatches = [match.group() for match in pastprogPattern.finditer(alice) if match]
+
+# Q4
+from nltk.corpus import gutenberg
+leaves = gutenberg.raw("whitman-leaves.txt")
+participlePattern = re.compile(r"(\w+)'(d)")
+leavesNorm = participlePattern.sub(r"\g<1>e\g<2>", leaves)
+
+# Q5
+willPattern = re.compile(r"(\w+)'(ll\W)")
+leavesNorm = willPattern.sub(r"\g<1>\swi\g<2>", leavesNorm)
+
+# Q6
+untokenized = " ".join(w for w in nltk.corpus.treebank.words() if not w.startswith("*") and w is not "0")
+tokenized = nltk.regexp_tokenize(text=untokenized, pattern=r"[A-Za-z]+|--|[.,?]|\$ [0-9]+(?:.[0-9]+)? (?:million|billion|thousand)?")
