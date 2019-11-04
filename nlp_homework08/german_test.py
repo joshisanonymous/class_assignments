20cf81739eed8252e9bbeac1a5a0c80650085cea
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-11-04 18:58:37 -0500

    Finished German grammar problem

--- nlp_homework08/german_test.py
+++ nlp_homework08/german_test.py
@@ -1,16 +1,17 @@
+# This tests several sentences using a feature-based grammar of German
+
 import nltk
 from nltk import load_parser
 
-
 sentences = [
     "der Hunde kommt".split(),
-    "der Hund kommt "
     "der Hund kommt".split(),
-    "uns moegen die Katze".split(),
-    "uns moegen".split()
-]
+    "der Hund kommt mir".split(),
+    "der Hund folgt mir".split()
+    ]
 chart_parser = load_parser("german.fcfg", trace=2)
 
 for sentence in sentences:
     for tree in chart_parser.parse(sentence):
         print(tree)
+        tree.draw()
