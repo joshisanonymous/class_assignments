5601edb7464bbbf18ea307b9fc3a26c8af99b23d
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2020-05-05 01:32:11 -0400

    Completed up to discussion, which has been outlined

--- race_ethnicity04/analysis.R
+++ race_ethnicity04/analysis.R
@@ -2,6 +2,7 @@
 library("plyr")
 library("readr")
 library("ggplot2")
+library("knitr")
 
 directory <- "./data/"
 files <- list.files(path = directory,
@@ -17,15 +18,21 @@ tokens$Speaker <- as.factor(tokens$Speaker)
 tokens$Origin <- as.factor(tokens$Origin)
 tokens$Gender <- as.factor(tokens$Gender)
 tokens$Race <- as.factor(tokens$Race)
+tokens$Citation <- as.factor(tokens$Citation)
+tokens$Stative <- as.factor(tokens$Stative)
 
 tokens$Origin <- gsub("False_River", "False River", tokens$Origin)
 tokens$Origin <- gsub("New_Roads", "New Roads", tokens$Origin)
 tokens$Origin <- gsub("Parlange Plantation", "Parlange", tokens$Origin)
 tokens$Race <- gsub("Creole_of_color", "Creole", tokens$Race)
 
-names(tokens) <- c(names(tokens[1:5]),
+names(tokens) <- c(names(tokens[1:6]),
                    "Birth Year",
-                   names(tokens[7:8]))
+                   names(tokens[8:10]))
+
+## ---- get_demographics ----
+demographics <- tokens[!duplicated(tokens$Speaker), c(4, 6:9)]
+row.names(demographics) <- NULL
 
 ## ---- freq_overall ----
 ggplot(tokens,
@@ -54,6 +61,23 @@ ggplot(tokens,
 ggplot(tokens,
        aes(x = Origin,
            fill = Short)) +
+  theme_bw() +
+  labs(y = axis_count) +
+  geom_bar(position = position_dodge(preserve = "single"))
+
+## ---- freq_verb ----
+ggplot(tokens,
+       aes(x = Stative,
+           fill = Short)) +
+  theme_bw() +
+  labs(y = axis_count) +
+  geom_bar(position = position_dodge(preserve = "single"))
+
+## ---- freq_race_speaker ----
+ggplot(tokens,
+       aes(x = Speaker,
+           fill = Short)) +
+  facet_grid(Race ~ .) +
   theme_bw() +
   labs(y = axis_count) +
   geom_bar(position = position_dodge(preserve = "single"))
